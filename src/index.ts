f567f8f4f5511e3ff2f03a5d4c231a81e2a4dfe9
homoluctus <w.slife18sy@gmail.com>
2021-05-12 12:36:58 +0900

    refactor: Separate github operation from slack block class
    - separation of responsibilities
    - easy-to-test code

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,55 @@
 import * as core from '@actions/core';
 import {IncomingWebhookDefaultArguments} from '@slack/webhook';
-
+import * as github from './github';
 import {validateStatus, isValidCondition} from './utils';
 import {Slack} from './slack';
 
 async function run() {
-  const status: string = validateStatus(
+  const status = validateStatus(
     core.getInput('type', {required: true}).toLowerCase()
   );
-  const jobName: string = core.getInput('job_name', {required: true});
-  const url: string = process.env.SLACK_WEBHOOK || core.getInput('url');
-  let mention: string = core.getInput('mention');
-  let mentionCondition: string = core.getInput('mention_if').toLowerCase();
+  const jobName = core.getInput('job_name', {required: true});
+  const url = process.env.SLACK_WEBHOOK || core.getInput('url');
+  let mention = core.getInput('mention');
+  let mentionCondition = core.getInput('mention_if').toLowerCase();
   const slackOptions: IncomingWebhookDefaultArguments = {
     username: core.getInput('username'),
     channel: core.getInput('channel'),
     icon_emoji: core.getInput('icon_emoji')
   };
-  const commitFlag: boolean = core.getInput('commit') === 'true';
-  const token: string = core.getInput('token');
+  const commitFlag = core.getInput('commit') === 'true';
+  const token = core.getInput('token');
 
   if (mention && !isValidCondition(mentionCondition)) {
     mention = '';
     mentionCondition = '';
-    core.warning(`
-      Ignore slack message metion:
+    core.warning(`Ignore slack message metion:
       mention_if: ${mentionCondition} is invalid
       `);
   }
 
-  if (url === '') {
-    throw new Error(`[Error] Missing Slack Incoming Webhooks URL.
+  if (!url) {
+    throw new Error(`Missing Slack Incoming Webhooks URL.
       Please configure "SLACK_WEBHOOK" as environment variable or
       specify the key called "url" in "with" section.
       `);
   }
 
-  const slack = new Slack();
-  const payload = await slack.generatePayload(
+  let commit: github.CommitContext | undefined;
+  if (commitFlag) {
+    commit = await github.getCommit(token);
+  }
+
+  const payload = Slack.generatePayload(
     jobName,
     status,
     mention,
     mentionCondition,
-    commitFlag,
-    token
+    commit
   );
   core.debug(`Generated payload for slack: ${JSON.stringify(payload)}`);
 
-  await slack.notify(url, slackOptions, payload);
+  await Slack.notify(url, slackOptions, payload);
   core.info('Post message to Slack');
 }
 
